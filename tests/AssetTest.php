9f4d56e05cdbc5dee731c88841eed77d87ad7d36
Wilmer Arambula <terabytesoftw@gmail.com>
2020-10-30 10:47:14 -0300

    Enhacements.

---
+++ tests/AssetTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Bootstrap4\Tests;
+
+use Yiisoft\Yii\Bootstrap4\Assets\BootstrapAsset;
+use Yiisoft\Yii\Bootstrap4\Assets\JqueryAsset;
+
+final class AssetTest extends TestCase
+{
+    /**
+     * @return array
+     */
+    public function registerDataProvider(): array
+    {
+        return [
+            [
+                'Css',
+                BootstrapAsset::class,
+            ],
+            [
+                'Js',
+                JqueryAsset::class,
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider registerDataProvider
+     *
+     *
+     * @param string $type
+     * @param string $asset
+     * @param string|null $depend
+     */
+    public function testAssetRegister(string $type, string $asset, ?string $depend = null): void
+    {
+        $publisher = $this->assetManager->getPublisher();
+
+        $bundle = new $asset();
+
+        if ($depend !== null) {
+            $depend = new $depend();
+        }
+
+        self::assertEmpty($this->assetManager->getAssetBundles());
+
+        $this->assetManager->register([$asset]);
+
+        if ($type === 'Css') {
+            if ($depend !== null) {
+                $dependUrl = $publisher->getPublishedUrl($depend->sourcePath) . '/' . $depend->css[0];
+                self::assertEquals($dependUrl, $this->assetManager->getCssFiles()[$dependUrl]['url']);
+            } else {
+                $bundleUrl = $publisher->getPublishedUrl($bundle->sourcePath) . '/' . $bundle->css[0];
+                self::assertEquals($bundleUrl, $this->assetManager->getCssFiles()[$bundleUrl]['url']);
+            }
+        }
+
+        if ($type === 'Js') {
+            if ($depend !== null) {
+                $dependUrl = $publisher->getPublishedUrl($depend->sourcePath) . '/' . $depend->js[0];
+                self::assertEquals($dependUrl, $this->assetManager->getJsFiles()[$dependUrl]['url']);
+            } else {
+                $bundleUrl = $publisher->getPublishedUrl($bundle->sourcePath) . '/' . $bundle->js[0];
+                self::assertEquals($bundleUrl, $this->assetManager->getJsFiles()[$bundleUrl]['url']);
+            }
+        }
+    }
+}
